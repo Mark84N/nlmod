202a4d3836269d547c3525e7a86afd709fa76fd8
dieiev.dmitry <dieiev.dima@gmail.com>
2018-12-04 20:41:16 +0200

    add family, ops and function stubs

--- src/nlmod.c
+++ src/nlmod.c
@@ -11,14 +11,85 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dmytro Dieiev");
 MODULE_DESCRIPTION("dummy nl80211 module");
 
-static int __init nlmod__init(void)
+struct nla_policy nlmod_nla_policy[NLMODULE_MAX + 1] = {
+    [NLMODULE_STR] = { .type = NLA_NUL_STRING, .len = 255, },
+    [NLMODULE_U32] = { .type = NLA_U32, },
+};
+
+static struct genl_family nlmod_family = {
+    .id = GENL_ID_GENERATE,
+    .hdrsize = NLMODULE_HDR_SIZE,
+    .name = NLMOD_CUSTOM_NAME,
+    .version = 0.1,
+    .maxattr = NLMODULE_MAX,
+};
+
+static int nlmod_get_str(struct sk_buff *skb, struct genl_info *info)
 {
     return 0;
 }
 
+static int nlmod_set_str(struct sk_buff *skb, struct genl_info *info)
+{
+    return 0;
+}
+
+static int nlmod_get_int(struct sk_buff *skb, struct genl_info *info)
+{
+    return 0;
+}
+
+static int nlmod_set_int(struct sk_buff *skb, struct genl_info *info)
+{
+    return 0;
+}
+
+static struct genl_ops nlmod_ops[] = {
+    {
+        .cmd = NLMODULE_GET_STR,
+        .flags = 0,
+        .policy = nlmod_nla_policy,
+        .doit = nlmod_get_str,
+        .dumpit = NULL,
+    },
+    {
+        .cmd = NLMODULE_SET_STR,
+        .flags = 0,
+        .policy = nlmod_nla_policy,
+        .doit = nlmod_set_str,
+        .dumpit = NULL,
+    },
+    {
+        .cmd = NLMODULE_GET_INT,
+        .flags = 0,
+        .policy = nlmod_nla_policy,
+        .doit = nlmod_get_int,
+        .dumpit = NULL,
+    },
+    {
+        .cmd = NLMODULE_SET_INT,
+        .flags = 0,
+        .policy = nlmod_nla_policy,
+        .doit = nlmod_set_int,
+        .dumpit = NULL,
+    },
+};
+
+static int __init nlmod__init(void)
+{
+    int err = 0;
+    
+    err = genl_register_family_with_ops(&nlmod_family, nlmod_ops);
+    if (err)
+        pr_crit("%s - failed to register family\n", KBUILD_MODNAME);
+
+    return err;
+}
+
 static void __exit nlmod__deinit(void)
 {
-
+    if (genl_unregister_family(&nlmod_family))
+        pr_crit("%s - failed to unregister family\n", KBUILD_MODNAME);
 }
 
 module_init(nlmod__init);
--- src/nlmod_private.h
+++ src/nlmod_private.h
@@ -5,11 +5,18 @@
 #include <net/genetlink.h>
 /*#include "nlmod_common.h"*/
 
-enum nlmod_msg_type {
+enum nlmod_attr_type {
     NLMODULE_UNDEF,
     NLMODULE_STR,
     NLMODULE_U32,
     __NLMODULE_MAX,
+};
+
+enum nlmod_cmd {
+    NLMODULE_GET_STR,
+    NLMODULE_SET_STR,
+    NLMODULE_GET_INT,
+    NLMODULE_SET_INT,
 };
 
 struct nlmod_hdr {
@@ -25,18 +32,5 @@ struct nlmod_hdr {
 #define NLMOD_CUSTOM_NAME "NLMOD CUSTOM"
 #define NLMODULE_MAX (__NLMODULE_MAX - 1)
 #define NLMODULE_HDR_SIZE sizeof(struct nlmod_hdr)
-
-struct nla_policy nlmod_nla_policies[NLMODULE_MAX + 1] = {
-    [NLMODULE_STR] = { .type = NLA_NUL_STRING, .len = 255, },
-    [NLMODULE_U32] = { .type = NLA_U32, },
-};
-
-static struct genl_family nlmod_family = {
-    .id = GENL_ID_GENERATE,
-    .hdrsize = NLMODULE_HDR_SIZE,
-    .name = NLMOD_CUSTOM_NAME,
-    .version = 0.1,
-    .maxattr = NLMODULE_MAX,
-};
 
 #endif /* __NLMODULE_H__ */
