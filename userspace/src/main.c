a04d0fef596cc59305b01c83e0fd5b7654d12651
dieiev.dmitry <dieiev.dima@gmail.com>
2019-01-16 23:55:50 +0200

    userspace: slight main rework

--- userspace/src/main.c
+++ userspace/src/main.c
@@ -5,21 +5,26 @@
 
 int main(int argc, char **argv)
 {
-    int gnl_sock;
-    int gnl_fam;
+    int gnl_sock = -1, gnl_fam = -1;
+    int ret = 1;
 
-    if ((gnl_sock = create_gnl_sock()) < 0) {
-        perror("error");
-        return -1;
-    }
+    if ((gnl_sock = gnl_create_sock()) < 0)
+        goto out;
 
-    if ((gnl_fam = get_gnl_fam(gnl_sock)) < 0) {
+    if ((gnl_fam = gnl_get_fam(gnl_sock)) < 0)
+        goto out;
+
+    printf("Success: family#%d\n", gnl_fam);
+
+    if (gnl_test_cmd(gnl_sock) < 0)
+        goto out;
+
+    ret = 0;
+out:
+    if (ret != 0)
         perror("error");
+    if (gnl_sock != -1)
         close(gnl_sock);
-        return -1;
-    }
 
-    printf("Success: %d\n", gnl_fam);
-
-    return 0;
+    return ret;
 }
