b902ab1069cb85c8d667bf0b56c86c549b23468b
dieiev.dmitry <dieiev.dima@gmail.com>
2019-01-20 20:58:40 +0200

    Moved some definitions from .c to .h

--- userspace/include/gnl_app.h
+++ userspace/include/gnl_app.h
@@ -7,7 +7,36 @@
 
 #include "nlmod_common.h"
 
-/* get pointer to  */
+struct nl_msg {
+    struct nlmsghdr nlhdr;
+    struct genlmsghdr gnlhdr;
+    uint8_t payload[4096];
+};
+
+/*
+    Attributes are kept in iov member of cfg
+    to be appended then to the msg.
+*/
+struct gnl_attr {
+  int type;
+  int len;
+  void *data;
+};
+
+/*
+    An attempt of generalizing sending API.
+    Probably ugly, yet functional.
+*/
+struct gnl_msg_cfg {
+    int nlmsg_type;
+    int nlmsg_flags;
+    int gnl_cmd;
+    /* a pointer to the array of iov, each of which holds gnl_attr */
+    struct iovec *iov;
+    /* qty of iov strutures pointed by iov * above */
+    int iov_len;
+};
+
 #define GENLMSG_DATA(nlmsg) \
         ((unsigned char *)(nlmsg) + NLMSG_ALIGN(NLMSG_HDRLEN + GENL_HDRLEN))
 
@@ -45,12 +74,6 @@ static inline struct nlattr *nla_next(struct nlattr *nla, int *remaining)
 #define nla_for_each_attr(pos, len, rem) \
     for (rem = len; nla_ok(pos, rem); \
     pos = nla_next(pos, &(rem)))
-
-struct nl_msg {
-    struct nlmsghdr nlhdr;
-    struct genlmsghdr gnlhdr;
-    uint8_t payload[4096];
-};
 
 int gnl_create_sock(void);
 int gnl_get_fam(int gnl_sock);
